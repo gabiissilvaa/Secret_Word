e9a5386fdba84558cf3333f96c2343122917acf5
Gabriela Borba <gborbas.gb@gmail.com>
2025-08-21 09:45:54 -0300

    Estágios do jogo

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,10 +1,47 @@
+//CSS
 import './App.css';
-import StartScreen from './components/StartScreen';
+
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//Data
+import { wordsList } from './data/words';
+
+//Components
+import StartScreen from './components/Start/StartScreen';
+import Game from './components/Game/Game';
+import GameOver from './components/Game/Over/GameOver';
+
+const stages = [
+  {id: 1, name:"start"},
+  {id: 2, name:"game"},
+  {id: 3, name:"end"},
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  
+  // Iniciar o secret word
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  // Processar o input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // Iniciar jogo novamente
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }
